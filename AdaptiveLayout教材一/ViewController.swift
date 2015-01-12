6840f965e50c639bf9359f148b4fafe2ed17893d
liushengxu <781461513@qq.com>
2015-01-12 13:53:24 +0800

    Initial Commit

---
+++ AdaptiveLayout教材一/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveLayout教材一
+//
+//  Created by notebook37 on 15/1/12.
+//  Copyright (c) 2015年 51auto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
